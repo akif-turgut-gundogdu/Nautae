939faa8b017ca7ad2fb9ad7e4844c79102f4cf6f
akif-turgut-gundogdu <akif-turgut-gundogdu@users.noreply.github.com>
2017-09-08 02:27:27 +0300

    first commit

---
+++ NautaeWebApi/Connections/Conn.cs
@@ -0,0 +1,50 @@
+﻿using NautaeWebApi.Models;
+using Pomelo.Data.MySql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace NautaeWebApi.Connections
+{
+    public class Conn
+    {
+        private string connStr;
+        public Conn()
+        {
+
+            connStr = @"Database=nautae_sql;Data Source=eu-cdbr-azure-west-b.cloudapp.net;User Id=bbe1ad88d1e83f;Password=358165a5";
+        }
+        public List<users> userList()
+        {
+
+            List<users> allUsers = new List<users>();
+            using (MySqlConnection connMySql = new MySqlConnection(connStr))
+            {
+
+                using (MySqlCommand cmd = connMySql.CreateCommand())
+                {
+
+                    cmd.CommandText = "Select * from userstable";
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = connMySql;
+                    connMySql.Open();
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            allUsers.Add(new users { Id = reader.GetInt32(reader.GetOrdinal("Id")), userName = reader.GetString(reader.GetOrdinal("userName")), password = reader.GetString(reader.GetOrdinal("password")), email = reader.GetString(reader.GetOrdinal("email")) });
+
+
+                        }
+                    }
+                }
+                connMySql.Close();
+
+            }
+            return allUsers;
+        }
+    }
+}
---
+++ NautaeWebApi/Models/users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NautaeWebApi.Models
+{
+    public class users
+    {
+        public int Id { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string comments { get; set; }
+    }
+}
